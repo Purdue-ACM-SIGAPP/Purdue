27a9ba2f77ac06e764c3e09a60d717102c9f0e0e
Mike Hockerman <mike@hockerman.com>
2015-01-22 17:07:27 -0500

    Implemented task to download and parse a single dining menu

---
+++ app/src/main/java/edu/purdue/app/dining/tasks/GetDailyDiningMenuTask.java
@@ -0,0 +1,90 @@
+package edu.purdue.app.dining.tasks;
+
+import android.os.AsyncTask;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.joda.time.LocalDate;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import edu.purdue.app.dining.models.DailyMenu;
+import edu.purdue.app.dining.models.Location;
+import edu.purdue.app.dining.util.LocationName;
+import edu.purdue.app.tasks.GetJsonApiTask;
+import edu.purdue.app.utility.Logger;
+
+/**
+ *  Asynchronously returns a DailyMenu object for a specific day and location on campus
+ *  Can optionally return the DailyMenu object through a callback inside this class
+ *  Created by mike on 1/22/15.
+ */
+public class GetDailyDiningMenuTask extends AsyncTask<Void, Void, DailyMenu> {
+
+    /** Callback interface for this task */
+    public interface DailyDiningMenuListener {
+        public void onResult(DailyMenu menu, Exception ex);
+    }
+
+    private LocalDate date;
+    private LocationName locationName;
+    private DailyDiningMenuListener listener;
+    private Exception exception;
+
+    public GetDailyDiningMenuTask(LocalDate date, LocationName location) {
+        this.date = date;
+        this.locationName = location;
+    }
+
+    public GetDailyDiningMenuTask(LocalDate date, LocationName locationName, DailyDiningMenuListener listener) {
+        this(date, locationName);
+        this.listener = listener;
+    }
+
+    @Override
+    protected DailyMenu doInBackground(Void... params) {
+
+        try {
+
+            // Construct the URL
+            URL url = locationName.createApiUrl(date);
+
+            // Map to objects using jackson
+            ObjectMapper mapper = new ObjectMapper();
+            DailyMenu menu = mapper.readValue(url, DailyMenu.class);
+            return menu;
+
+        } catch (MalformedURLException ex) {
+            Logger.e(ex);
+            exception = ex;
+
+        } catch (JsonParseException ex) {
+            Logger.e(ex);
+            exception = ex;
+
+        } catch (JsonMappingException ex) {
+            Logger.e(ex);
+            exception = ex;
+
+        } catch (IOException ex) {
+            Logger.e(ex);
+            exception = ex;
+
+        }
+
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(DailyMenu menu) {
+        if (listener != null) {
+            listener.onResult(menu, exception);
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/purdue/app/dining/util/LocationName.java
@@ -0,0 +1,48 @@
+package edu.purdue.app.dining.util;
+
+import org.joda.time.LocalDate;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *  Every dining court location that we currently recognize on campus.
+ *  Ideally the functionality from this class should be provided by some call to
+ *  /api/v2/locations, but considering how unoften dining court locations are
+ *  updated and how much slower that would make the app this is probably a good
+ *  compromise.
+ *
+ *  Created by mike on 1/22/15.
+ */
+public enum LocationName {
+
+    EARHART     ("Earhart",     "earhart"),
+    FORD        ("Ford",        "ford"),
+    HILLENBRAND ("Hillenbrand", "hillenbrand"),
+    WILEY       ("Wiley",       "wiley"),
+    WINDSOR     ("Windsor",     "windsor");
+
+    private String printableName;
+    private String urlComponent;
+
+    LocationName(String printable, String urlComponent) {
+        this.printableName = printable;
+        this.urlComponent = urlComponent;
+    }
+
+    public String printableName() {
+        return printableName;
+    }
+
+    public String urlComponent() {
+        return urlComponent;
+    }
+
+    /** Creates the url for a given date and the location you are trying to access */
+    public URL createApiUrl(LocalDate date) throws MalformedURLException {
+        String urlStr = "http://api.hfs.purdue.edu/menus/v2/locations/" +
+                urlComponent() + "/" + date.toString("yyyy-MM-dd");
+        return new URL(urlStr);
+    }
+
+}
