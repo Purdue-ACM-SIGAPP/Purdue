6cb2d453ab63ebacd9e81f8b0d0be9c4dffede80
Jordan Field <fieldn@purdue.edu>
2015-02-02 19:07:32 -0500

    Added comments to NewsAdapter.

--- app/src/main/java/edu/purdue/app/news/NewsAdapter.java
+++ app/src/main/java/edu/purdue/app/news/NewsAdapter.java
@@ -21,6 +21,7 @@ public class NewsAdapter extends ArrayAdapter<String> {
     private final ArrayList<String> web;
     private final Integer[] imageId;
 
+    // Constructor for custom adapter; @param activity; @param String arraylist; @param int array
     public NewsAdapter(Activity context, ArrayList<String> web, Integer[] imageId) {
 
         super(context, R.layout.news_item_grid, web);
@@ -32,17 +33,18 @@ public class NewsAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View view, ViewGroup parent) {
 
-        LayoutInflater inflater = context.getLayoutInflater();
+        LayoutInflater inflater = context.getLayoutInflater(); //Inflates activity
 
+        //Prevents multiple inflations of the same layout for faster scrolling
         if (view == null) {
             view = inflater.inflate(R.layout.news_item_grid, null, false);
         }
 
-        TextView txtTitle = (TextView) view.findViewById(R.id.txt);
-        ImageView imageView = (ImageView) view.findViewById(R.id.img);
+        TextView txtTitle = (TextView) view.findViewById(R.id.txt); // Grabs title id from view
+        ImageView imageView = (ImageView) view.findViewById(R.id.img); // Grabs icon id from view
 
-        txtTitle.setText(web.get(position));
-        imageView.setImageResource(imageId[position]);
+        txtTitle.setText(web.get(position)); // Sets text in view to grabbed title
+        imageView.setImageResource(imageId[position]); // Sets image in view to grabbed icon
 
         return view;
     }
