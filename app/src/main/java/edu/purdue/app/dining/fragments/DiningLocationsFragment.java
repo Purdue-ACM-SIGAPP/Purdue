58e7cbf6886d26b51ee188f31c7e56b05b19c2a3
Mike Hockerman <mike@hockerman.com>
2015-02-05 17:23:59 -0500

    Added multi-select capability to locations list

--- app/src/main/java/edu/purdue/app/dining/fragments/DiningLocationsFragment.java
+++ app/src/main/java/edu/purdue/app/dining/fragments/DiningLocationsFragment.java
@@ -7,10 +7,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.purdue.app.R;
 import edu.purdue.app.dining.data.DiningData;
@@ -23,10 +26,15 @@ import edu.purdue.app.widgets.CardViewListAdapter;
 /**
  * Created by mike on 2/5/15.
  */
-public class DiningLocationsFragment extends Fragment implements LocationsListener {
+public class DiningLocationsFragment extends Fragment implements LocationsListener, AdapterView.OnItemClickListener {
 
     private GridView locationsGrid;
+    private Set<Integer> selectedLocations;
     private OnLoadedListener loadedListener;
+
+    public DiningLocationsFragment() {
+        this.selectedLocations = new HashSet<>();
+    }
 
     @Nullable
     @Override
@@ -37,6 +45,7 @@ public class DiningLocationsFragment extends Fragment implements LocationsListen
 
         // Get elements from the view
         locationsGrid = (GridView) fragmentView.findViewById(R.id.dining_master_gridview);
+        locationsGrid.setOnItemClickListener(this);
 
         return fragmentView;
     }
@@ -67,6 +76,24 @@ public class DiningLocationsFragment extends Fragment implements LocationsListen
 
     public void setOnLoadedListener(OnLoadedListener loadedListener) {
         this.loadedListener = loadedListener;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        if (selectedLocations.contains(position)) {
+            selectedLocations.remove(position);
+
+            // Remove the highlighting from the view
+            view.setBackgroundColor(getResources().getColor(android.R.color.white));
+
+        } else {
+            selectedLocations.add(position);
+
+            // Add highlighting to the view
+            view.setBackgroundColor(getResources().getColor(R.color.black40));
+        }
+
     }
 
 }
